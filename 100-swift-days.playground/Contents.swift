7783026a1932fb9c549011145c65785567c59272
Javier Castaneda <jdandini.jc@gmail.com>
2026-06-16 11:22:16 -0600

    Initial commit

---
+++ 100-swift-days.playground/Contents.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+var greeting = "Hello, playground"
+
+var name = "Ted"
+name = "Rebecca"
+name = "Keeley"
+
+let character = "Daphne"
+
+var playerName = "Javi"
+print(playerName)
+
+playerName = "Luis"
+print(playerName)
+
+playerName = "Gabriel"
+print(playerName)
