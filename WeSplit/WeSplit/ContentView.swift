26aba4690defbbdf954cd828b70ebed58bff42b2
Javier Castaneda <jdandini.jc@gmail.com>
2026-06-18 11:58:49 -0600

    Add 4 sections, amount, tip percentage, total per person and grand total

--- WeSplit/WeSplit/ContentView.swift
+++ WeSplit/WeSplit/ContentView.swift
@@ -8,20 +8,75 @@
 import SwiftUI
 
 struct ContentView: View {
-    private let students =
-    [
-        "Harry",
-        "Ron",
-        "Hermione"
-    ]
-    @State private var selectedStudent: String = "Harry"
+    @State private var checkAmount = 0.0
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 0
+    @FocusState private var amountIsFocused: Bool
+    
+    private let locale = Locale.current.currency?.identifier ?? "USD"
+    private var totalPerPerson: Double {
+        let peopleCount = Double(numberOfPeople + 2)
+        return grandTotal / peopleCount
+    }
+    
+    private var grandTotal: Double {
+        let tipSelection = Double(tipPercentage)
+        let tipValue = checkAmount * tipSelection / 100
+        let total = checkAmount + tipValue
+        return total
+    }
     
     var body: some View {
         NavigationStack {
             Form {
-                Picker("Select your student", selection: $selectedStudent) {
-                    ForEach(students, id: \.self ) {
-                        Text($0)
+                Section {
+                    TextField(
+                        "Amount",
+                        value: $checkAmount,
+                        format: .currency(code:locale)
+                    )
+                    .keyboardType(.decimalPad)
+                    .focused($amountIsFocused)
+                    Picker("Number of people", selection: $numberOfPeople) {
+                        ForEach(2 ..< 100) {
+                            Text("\($0) people")
+                        }
+                    }
+                }
+                Section {
+                    Picker("Number of people", selection: $tipPercentage) {
+                        ForEach(0 ..< 101) {
+                            Text("\($0)")
+                        }
+                    }
+                    .pickerStyle(.navigationLink)
+                } header: {
+                    Text("How much tip do you want to leave?")
+                }
+            
+                Section {
+                    Text(
+                        totalPerPerson,
+                        format: .currency(code: locale)
+                    )
+                } header: {
+                    Text("Total per person")
+                }
+                
+                Section {
+                    Text(
+                        grandTotal,
+                        format: .currency(code: locale)
+                    )
+                } header: {
+                    Text("Total amount to pay")
+                }
+            }
+            .navigationTitle("We Split")
+            .toolbar {
+                if amountIsFocused {
+                    Button("Done") {
+                        amountIsFocused = false
                     }
                 }
             }
