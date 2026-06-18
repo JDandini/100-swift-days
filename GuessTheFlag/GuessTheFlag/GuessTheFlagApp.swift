9f79ae5423c8145a64931d2e94a99a9710dd3daf
Javier Castaneda <jdandini.jc@gmail.com>
2026-06-18 12:07:06 -0600

    Add The guess the flag project

---
+++ GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Javier Castañeda on 18/06/26.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
