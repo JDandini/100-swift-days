da8ef2437446d5e02d08e2bf006f5fb2a15a216e
Javier Castaneda <jdandini.jc@gmail.com>
2026-06-19 13:57:16 -0600

    Creating custom button with the Label property

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -9,16 +9,13 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Button("Button 1"){}
-                .buttonStyle(.bordered)
-            Button("Button 2", role: .destructive){}
-                .buttonStyle(.bordered)
-            Button("Button 3"){}
-                .buttonStyle(.borderedProminent)
-                .tint(.teal)
-            Button("Button 4", role: .destructive){}
-                .buttonStyle(.borderedProminent)
+        Button {
+            print("Button tapped")
+        } label: {
+            Text("Tap me!")
+                .padding()
+                .foregroundStyle(.white)
+                .background(.teal)
         }
     }
     
