28caeb8c4cf5e0cb315fcabea2f41d51e2f2d006
Javier Castaneda <jdandini.jc@gmail.com>
2026-06-23 09:59:14 -0600

    BetterRest Project creation

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Javier Castañeda on 23/06/26.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
