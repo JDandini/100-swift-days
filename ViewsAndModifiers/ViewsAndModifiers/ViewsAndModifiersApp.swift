2d8f15a35084cf3dd5e6af3363868890dfa9e305
Javier Castaneda <jdandini.jc@gmail.com>
2026-06-23 06:22:33 -0600

    Starting ViewsAndModifiers project

---
+++ ViewsAndModifiers/ViewsAndModifiers/ViewsAndModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewsAndModifiersApp.swift
+//  ViewsAndModifiers
+//
+//  Created by Javier Castañeda on 23/06/26.
+//
+
+import SwiftUI
+
+@main
+struct ViewsAndModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
