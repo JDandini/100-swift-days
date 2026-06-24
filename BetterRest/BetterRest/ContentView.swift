9e9bbfd79d4fd0c822fa931f313ad5ee102fda25
Javier Castaneda <jdandini.jc@gmail.com>
2026-06-24 08:18:33 -0600

    Building a basic layout for the better sleep using date picker, steppers and texts

--- BetterRest/BetterRest/ContentView.swift
+++ BetterRest/BetterRest/ContentView.swift
@@ -8,24 +8,51 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var sleepAmount = 8.0
+    @State private var sleepAmount: Double = 8.0
+    @State private var coffeeAmount: Int = 1
     @State private var wakeUp = Date.now
+    
     var body: some View {
-        VStack {
-            DatePicker(
-                "Please enter a date",
-                selection: $wakeUp,
-                in: Date.now...
-            )
-            .labelsHidden()
-            Stepper(
-             "\(sleepAmount.formatted()) hours",
-             value: $sleepAmount,
-             in: 4...12,
-             step: 0.25
-            )
+        NavigationStack {
+            VStack {
+                Text("When do you want to wake up?")
+                    .font(.headline)
+                DatePicker(
+                    "Please enter a time",
+                    selection: $wakeUp,
+                    displayedComponents: .hourAndMinute
+                )
+                .labelsHidden()
+                
+                Text("Desired amount of sleep")
+                    .font(.headline)
+                Stepper(
+                    "\(sleepAmount.formatted()) hours",
+                    value: $sleepAmount,
+                    in: 4...12,
+                    step: 0.25
+                )
+                
+                Text("Daily coffee intake")
+                    .font(.headline)
+                Stepper(
+                    "\(coffeeAmount) cups",
+                    value: $coffeeAmount,
+                    in: 0...20,
+                )
+                
+            }
+            .padding()
+            .navigationTitle("Better Sleep")
+            .toolbar {
+                Button("Calculate", action: calculateBedTime)
+            }
         }
-        .padding()
+       
+    }
+    
+    func calculateBedTime() {
+        
     }
     
 }
