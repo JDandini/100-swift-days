bcc0d79e335cb01ff1dee643ff78edd3a0818e5a
Javier Castaneda <jdandini.jc@gmail.com>
2026-06-25 06:44:25 -0600

    Add WordScramble project

---
+++ WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Javier Castañeda on 25/06/26.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
