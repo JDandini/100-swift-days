0c9bda8904e5385f35c639b49215ca3bac80f1bc
Javier Castaneda <jdandini.jc@gmail.com>
2026-06-26 06:51:16 -0600

    Create the animations project

---
+++ Animations/Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Javier Castañeda on 26/06/26.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
