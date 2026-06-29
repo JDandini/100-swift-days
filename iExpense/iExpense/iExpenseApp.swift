06220113e01db20edc217e7d4d75e7c2101e3e57
Javier Castaneda <jdandini.jc@gmail.com>
2026-06-29 11:19:09 -0600

    Create the iExpense project

---
+++ iExpense/iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Javier Castañeda on 29/06/26.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
