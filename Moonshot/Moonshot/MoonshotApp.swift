68da48a1170d8a968846f6621604739b89bb42e5
Javier Castaneda <jdandini.jc@gmail.com>
2026-07-02 08:05:01 -0600

    Adding the Moonshot project to the course

---
+++ Moonshot/Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Javier Castañeda on 02/07/26.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
