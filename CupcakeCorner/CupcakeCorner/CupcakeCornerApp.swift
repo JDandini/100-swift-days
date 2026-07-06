34b74f7234cd1dadcc353b953904b150de4bcca6
Javier Castaneda <jdandini.jc@gmail.com>
2026-07-06 15:36:28 -0600

    Adding CupcakeCorner project to the learning path

---
+++ CupcakeCorner/CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by F J Castaneda Ramos on 06/07/26.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
