de89ec9d0459ef5a356c01257e9ad64bbeeb7b01
Javier Castaneda <jdandini.jc@gmail.com>
2026-07-07 06:46:03 -0600

    Validating and disabling forms

--- CupcakeCorner/CupcakeCorner/ContentView.swift
+++ CupcakeCorner/CupcakeCorner/ContentView.swift
@@ -8,47 +8,25 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var results = [Result]()
-    var body: some View {
-        VStack {
-            AsyncImage(url: URL(string: "https://hws.dev/img/logo.png")) { phase in
-                if let image = phase.image {
-                    image
-                        .resizable()
-                        .scaledToFit()
-                } else if phase.error != nil {
-                    Text("There was an error loading the image.")
-                } else {
-                    ProgressView()
-                }
-            }
-            .frame(width: 200, height: 200)
-            List(results, id: \.trackId) { item in
-                VStack(alignment: .leading) {
-                    Text(item.trackName)
-                        .font(.headline)
-                    Text(item.collectionName)
-                }
-            }
-            .task {
-                await loadData()
-            }
-        }
+    @State private var username = ""
+    @State private var email = ""
+    var disableForm: Bool {
+        username.count < 5 || email.count < 5
     }
-    
-    func loadData() async {
-        guard let url = URL(string: "https://itunes.apple.com/search?term=taylor+swift&entity=song") else {
-            print("Invalid URL")
-            return
-        }
-        do {
-            let (data, _) = try await URLSession.shared.data(from: url)
 
-            if let decodedResponse = try? JSONDecoder().decode(Response.self, from: data) {
-                results = decodedResponse.results
+    var body: some View {
+        Form {
+            Section {
+                TextField("Username", text: $username)
+                TextField("Email", text: $email)
             }
-        } catch {
-            print("Invalid data")
+
+            Section {
+                Button("Create account") {
+                    print("Creating account…")
+                }
+            }
+            .disabled(disableForm)
         }
     }
 }
@@ -57,11 +35,3 @@ struct ContentView: View {
     ContentView()
 }
 
-struct Response: Codable {
-    var results: [Result]
-}
-struct Result: Codable {
-    var trackId: Int
-    var trackName: String
-    var collectionName: String
-}
