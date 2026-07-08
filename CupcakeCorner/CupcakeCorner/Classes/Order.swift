dcded8da8f8a8374689cdb3764d4a3b824ab9c1c
Javier Castaneda <jdandini.jc@gmail.com>
2026-07-08 06:28:37 -0600

    Preparing for checkout

--- CupcakeCorner/CupcakeCorner/Classes/Order.swift
+++ CupcakeCorner/CupcakeCorner/Classes/Order.swift
@@ -9,6 +9,7 @@ import Foundation
 
 @Observable
 final class Order {
+    let unitPrice: Decimal = 2
     enum Flavor: String, CaseIterable {
         case vanilla
         case strawberry
@@ -19,11 +20,17 @@ final class Order {
             let value = self.rawValue
             return value.prefix(1).uppercased() + value.dropFirst()
         }
+
+        var extraCharge: Decimal {
+            switch self {
+            case .rainbow: return 1
+            default: return 0
+            }
+        }
     }
 
     var flavor: Flavor = .vanilla
     var quantity = 3
-
     var specialRequestEnabled = false {
         didSet {
             if specialRequestEnabled == false {
@@ -35,4 +42,18 @@ final class Order {
     var extraFrosting = false
     var addSprinkles = false
     var shippingAddress = Address()
+
+    var total: Decimal {
+        var cost = unitPrice * Decimal(quantity)
+        cost += (flavor.extraCharge / 2)
+        if extraFrosting {
+            cost += Decimal(quantity)
+        }
+
+        if addSprinkles {
+            cost += Decimal(quantity) / 2
+        }
+
+        return cost
+    }
 }
