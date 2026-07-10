3fd102572b5bb930667c9b3058b6e231ae220ca6
Javier Castaneda <jdandini.jc@gmail.com>
2026-07-09 19:50:14 -0600

    Create SwiftDataProject and add it to the course

---
+++ SwiftDataProject/SwiftDataProject/SwiftDataProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataProjectApp.swift
+//  SwiftDataProject
+//
+//  Created by Javier Castañeda on 09/07/26.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
