a4edaa61053bb2f01f67684a9c8197130bc14168
Javier Castaneda <jdandini.jc@gmail.com>
2026-07-16 11:14:20 -0600

    Editing SwiftData model objects

--- SwiftDataProject/SwiftDataProject/SwiftDataProjectApp.swift
+++ SwiftDataProject/SwiftDataProject/SwiftDataProjectApp.swift
@@ -5,6 +5,7 @@
 //  Created by Javier Castañeda on 09/07/26.
 //
 
+import SwiftData
 import SwiftUI
 
 @main
@@ -13,5 +14,6 @@ struct SwiftDataProjectApp: App {
         WindowGroup {
             ContentView()
         }
+        .modelContainer(for: User.self)
     }
 }
