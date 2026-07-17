1d0e5c84e285fb9856dd87b3c953815d1870c873
Javier Castaneda <jdandini.jc@gmail.com>
2026-07-17 06:43:21 -0600

    Dynamically sorting and filtering @Query with SwiftUI

---
+++ SwiftDataProject/SwiftDataProject/Views/UsersView.swift
@@ -0,0 +1,29 @@
+//
+//  UsersView.swift
+//  SwiftDataProject
+//
+//  Created by Javier Castañeda on 17/07/26.
+//
+
+import SwiftData
+import SwiftUI
+
+struct UsersView: View {
+    @Query var users: [User]
+    var body: some View {
+        List(users) { user in
+            Text(user.name)
+        }
+    }
+    
+    init(minimumJoinDate: Date, sortOrder: [SortDescriptor<User>]) {
+        _users = Query(filter: #Predicate<User> { user in
+            user.joinDate >= minimumJoinDate
+        }, sort: \User.name)
+    }
+}
+
+#Preview {
+    UsersView(minimumJoinDate: .now, sortOrder: [SortDescriptor(\User.name)])
+        .modelContainer(for: User.self)
+}
