1874f96bd77faffbca54f5c607b04f9ef0b9fa88
Javier Castañeda <jdandini.jc@gmail.com>
2026-07-22 13:51:58 -0600

    Add the Instafilter app to the project

---
+++ Instafilter/Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Javier Castañeda on 22/07/26.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
