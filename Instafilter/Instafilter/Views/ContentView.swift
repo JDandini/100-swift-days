b71874eefc574714a1b96a42c7933ceef785a098
Javier Castañeda <jdandini.jc@gmail.com>
2026-08-10 08:19:37 -0600

    Importing an image into SwiftUI using PhotosPicker

--- Instafilter/Instafilter/Views/ContentView.swift
+++ Instafilter/Instafilter/Views/ContentView.swift
@@ -5,16 +5,21 @@
 //  Created by Javier Castañeda on 22/07/26.
 //
 
+import PhotosUI
 import SwiftUI
 
 struct ContentView: View {
     @State private var processedImage: Image?
     @State private var filterIntensity = 0.5
+    @State private var selectedItem: PhotosPickerItem?
     
     var body: some View {
         NavigationStack {
             VStack {
-                ImagePreview(processedImage: processedImage)
+                PhotosPicker(selection: $selectedItem) {
+                    ImagePreview(processedImage: processedImage)
+                }
+                .onChange(of: selectedItem, loadImage)
                 SliderIntensity(filterIntensity: $filterIntensity)
                 ButtonLeftAligned(
                     buttonText: "Change Filter",
@@ -28,6 +33,13 @@ struct ContentView: View {
     
     private func changeFilter() {
         
+    }
+    
+    private func loadImage() {
+        Task {
+            guard let imageData = try await selectedItem?.loadTransferable(type: Data.self),
+                  let inputImage = UIImage(data: imageData) else { return }
+        }
     }
 }
 
