c0ee47cd6e2f533898be6dff3a0927730e780b70
Javier Castañeda <jdandini.jc@gmail.com>
2026-08-13 12:22:35 -0600

    Add the AccessibilitySandbox project

---
+++ AccessibilitySandbox/AccessibilitySandbox/AccessibilitySandboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccessibilitySandboxApp.swift
+//  AccessibilitySandbox
+//
+//  Created by Javier Castañeda on 13/08/26.
+//
+
+import SwiftUI
+
+@main
+struct AccessibilitySandboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
