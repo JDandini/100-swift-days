0d99f6f9bc84e813a12a605f32968331cbd83759
Javier Castañeda <jdandini.jc@gmail.com>
2026-08-25 08:23:35 -0600

    How to be notified when your SwiftUI app moves to the background

--- Flashzilla/Flashzilla/ContentView.swift
+++ Flashzilla/Flashzilla/ContentView.swift
@@ -9,22 +9,20 @@ import Combine
 import SwiftUI
 
 struct ContentView: View {
+    @Environment(\.scenePhase) var scenePhase
 
-    let timer = Timer.publish(every: 1, tolerance: 0.5, on: .main, in: .common).autoconnect()
-    @State private var counter = 0
-
-    var body: some View {
-        Text("Hello, World!")
-            .onReceive(timer) { time in
-                if counter == 5 {
-                    timer.upstream.connect().cancel()
-                } else {
-                    print("The time is now \(time)")
-                }
-
-                counter += 1
-            }
-    }
+       var body: some View {
+           Text("Hello, world!")
+               .onChange(of: scenePhase) { oldPhase, newPhase in
+                   if newPhase == .active {
+                       print("Active")
+                   } else if newPhase == .inactive {
+                       print("Inactive")
+                   } else if newPhase == .background {
+                       print("Background")
+                   }
+               }
+       }
 }
 
 #Preview {
