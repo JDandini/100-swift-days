608cb74aa6624bde7defb3d909a092c7c266119c
Javier Castañeda <jdandini.jc@gmail.com>
2026-08-25 08:00:12 -0600

    How to use gestures in SwiftUI

---
+++ Flashzilla/Flashzilla/FlashzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashzillaApp.swift
+//  Flashzilla
+//
+//  Created by Javier Castañeda on 25/08/26.
+//
+
+import SwiftUI
+
+@main
+struct FlashzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
