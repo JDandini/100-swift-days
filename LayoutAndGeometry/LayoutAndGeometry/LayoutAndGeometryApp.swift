865f771c768be9103c30331cdf740d0f3f17b33e
Javier Castañeda <jdandini.jc@gmail.com>
2026-08-31 14:07:56 -0600

    Add the LayoutAndGeometry project

---
+++ LayoutAndGeometry/LayoutAndGeometry/LayoutAndGeometryApp.swift
@@ -0,0 +1,17 @@
+//
+//  LayoutAndGeometryApp.swift
+//  LayoutAndGeometry
+//
+//  Created by Javier Castañeda on 31/08/26.
+//
+
+import SwiftUI
+
+@main
+struct LayoutAndGeometryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
