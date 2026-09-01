e2d09ffb6ccd8df69d481e242c9719323599fcc3
Javier Castañeda <jdandini.jc@gmail.com>
2026-09-01 10:35:59 -0600

    Absolute positioning for SwiftUI views

--- LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
+++ LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
@@ -9,31 +9,10 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        HStack(alignment: .midAccountAndName) {
-            VStack {
-                Text("@twostraws")
-                Image(.paulHudson)
-                    .resizable()
-                    .frame(width: 64, height: 64)
-            }
-
-            VStack {
-                Text("Full name:")
-                Text("PAUL HUDSON")
-                    .font(.largeTitle)
-            }
-        }
+        Text("Hello, world!")
+            .offset(x: 100, y: 100)
+            .background(.red)
     }
-}
-
-extension VerticalAlignment {
-    struct MidAccountAndName: AlignmentID {
-        static func defaultValue(in context: ViewDimensions) -> CGFloat {
-            context[.top]
-        }
-    }
-
-    static let midAccountAndName = VerticalAlignment(MidAccountAndName.self)
 }
 
 #Preview {
