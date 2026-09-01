5d01662a1062ca6ff7d59c3f30e483a1c2ddc612
Javier Castañeda <jdandini.jc@gmail.com>
2026-09-01 10:32:24 -0600

    How to create a custom alignment guide

--- LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
+++ LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
@@ -9,16 +9,31 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack(alignment: .leading) {
-               ForEach(0..<10) { position in
-                   Text("Number \(position)")
-                       .alignmentGuide(.leading) { _ in Double(position) * -10 }
-               }
-           }
-           .background(.red)
-           .frame(width: 400, height: 400)
-           .background(.blue)
+        HStack(alignment: .midAccountAndName) {
+            VStack {
+                Text("@twostraws")
+                Image(.paulHudson)
+                    .resizable()
+                    .frame(width: 64, height: 64)
+            }
+
+            VStack {
+                Text("Full name:")
+                Text("PAUL HUDSON")
+                    .font(.largeTitle)
+            }
+        }
     }
+}
+
+extension VerticalAlignment {
+    struct MidAccountAndName: AlignmentID {
+        static func defaultValue(in context: ViewDimensions) -> CGFloat {
+            context[.top]
+        }
+    }
+
+    static let midAccountAndName = VerticalAlignment(MidAccountAndName.self)
 }
 
 #Preview {
